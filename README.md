409cace2d97dd6f20598eaa7d126d562c1f09eec
Dan Kantor <dan.kantor@gmail.com>
2014-03-31 12:04:39 -0400

    initial import

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ itunes-parser
 =============
 
 Parse iTunes Music Library.xml into Array of objects
+
+## Install
+
+    npm install itunes-parser
+
+
+## Usage
+
+    var itunesParser = require('itunes-parser');
+    var songs = itunesParser.parse(xml);
+    
+    or to include and map to different properties:
+    
+    var songs = itunesParser.parse(
+            xml,
+            {
+                'Track ID': 'id',
+                'Artist': 'artist',
+                'Album': 'album',
+                'Genre': 'genre',
+                'Name': 'title',
+                'Location': 'url'
+            }
+        );
---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports.parse = function(xml, map) {
+    if(_.isString(xml) === true){
+        var parser = new DOMParser();
+        xml = parser.parseFromString(xml,"text/xml");
+    }
+    var songs = [];
+    var root = xml.getElementsByTagName('plist')[0];
+    var dicts = root.getElementsByTagName('dict');
+    var tracksDict = dicts[1];
+    var trackDicts = tracksDict.getElementsByTagName('dict');
+    _.each(
+        trackDicts, 
+        function(dict){
+            var keys = dict.getElementsByTagName('key');
+            var song = {};
+            _.each(
+                keys,
+                function(key){
+                    var innerHTML = key.innerHTML;
+                    var nextSiblingInnerHTML = key.nextSibling.innerHTML;
+                    if(map){
+                        if(map[innerHTML]){
+                            song[map[innerHTML]] = nextSiblingInnerHTML;
+                        }
+                    }
+                    else{
+                        song[innerHTML] = nextSiblingInnerHTML;
+                    }
+                }.bind(this)
+            );
+            songs.push(song);
+        }.bind(this)
+    );
+    return songs;
+}
